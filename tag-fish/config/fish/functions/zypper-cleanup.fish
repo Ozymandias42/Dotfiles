6b4bf094aa1b1c3626182dafa3b235e02fe6ab2c
AppleFoxUser42 <fabian_otto_de@yahoo.de>
2018-08-30 11:01:48 +0200

    after three tries it's how it should be

--- tag-fish/config/fish/functions/zypper-cleanup.fish
+++ tag-fish/config/fish/functions/zypper-cleanup.fish
@@ -1,7 +1,7 @@
 function zypper-cleanup
   set thingsToClean (zypper pa --unneeded|tail -n+5|awk -F'|' '{print $3}')
   if expr (count $thingsToClean) '>' 2
-    xargs sudo zypper rm -u $thingsToClean
+    echo $thingsToClean|xargs sudo zypper rm -u 
   else
     echo "Nothing to cleanup"
   end
