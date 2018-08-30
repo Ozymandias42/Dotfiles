bca8b1c0b4dc966d5d20fa8feae0aee3292f0862
AppleFoxUser42 <fabian_otto_de@yahoo.de>
2018-08-30 10:33:27 +0200

    added zypper-cleanup function to fish

---
+++ tag-fish/config/fish/functions/zypper-cleanup.fish
@@ -0,0 +1,9 @@
+function zypper-cleanup
+  set thingsToClean (zypper pa --unneeded|tail -n+5|awk -F'|' '{print $3}')
+  if expr (count $thingsToClean) '>' 2
+    #xargs sudo zypper rm -u $thingsToClean
+    echo $thingsToClean
+  else
+    echo "Nothing to cleanup"
+  end
+end
